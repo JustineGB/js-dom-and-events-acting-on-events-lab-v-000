f20e29493e9eb82ed6c57b0d08bb9bc194a84e1c
Justine B <justinegayton@gmail.com>
2019-04-01 19:56:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,42 @@ function preventRefreshOnSubmit(){
       event.preventDefault()
     })
 }
-
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation(input){
+  let employee = document.querySelector('input').value
+  return employee;
+}
+
+
+function addNewElementAsLi(){
+  $('ul.employee-list').append(retrieveEmployeeInformation());
+}
+
+function addNewLiOnClick(event){
+  let submit = $('form input:last')[0];
+  submit.addEventListener('click', event => {
+    addNewElementAsLi();
+    //$('input').reset();
+  //let name = document.querySelector('input').value
+  //name.reset();
+  //return false;
+  })
+ 
+}
+
+function extractValue() {
+    var value = $("#submit").val().trim();
+    console.log(value);
+};
+
+
+function clearEmployeeListOnLinkClick() {
+  let x = document.querySelector('#clear')
+  x.addEventListener('click', event => { event.preventDefault();
+  $('ul').innerHTML = '';
+  })
+}
+
+
